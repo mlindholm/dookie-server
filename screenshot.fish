0c0aae91f47514097088915176fd7c54dae654dd
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-02-23 21:48:42 +0200

    Fix screenshot script

--- screenshot.fish
+++ screenshot.fish
@@ -3,8 +3,7 @@
 echo "Insert dates into json"
 set today (date "+%Y-%m-%d")
 set yesterday (date -v -1d "+%Y-%m-%d")
-sed -i ".bak" "s/TODAY/$today/g" screenshot.data.json
-sed -i ".bak" "s/YESTERDAY/$yesterday/g" screenshot.data.json
+sed -i ".bak" "s/TODAY/$today/g;s/YESTERDAY/$yesterday/g" screenshot.data.json
 echo "Push to firebase"
 firebase database:set "/-KdbklMPTqReQ-OFr-v7" screenshot.data.json -y
 mv screenshot.data.json.bak screenshot.data.json
