b7e54e96548aaa650334f3987f59013185dab584
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-05-10 23:11:49 +0300

    Log new activities

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,10 @@ var functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+exports.activityAdded = functions.database.ref('/{pet}/activities/{pushId}')
+    .onWrite(event => {
+        const object = event.data;
+        console.log('Object', object.val())
+        // console.log('Parent', object.ref.parent)
+        // console.log('Data', event.params.pushId, object.val());
+    });
