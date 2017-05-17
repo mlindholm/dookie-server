65739a94cb474bd8266c78de3628a49c850df3fd
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-05-17 23:48:28 +0300

    Run removeOldActivities when user comes online

--- functions/index.js
+++ functions/index.js
@@ -4,23 +4,19 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.removeOldActivities = functions.database.ref('/{petId}/activities/{pushId}').onWrite(event => {
-    const activitiesRef = event.data.ref.parent;
-    const lastmodifiedRef = activitiesRef.parent.child('lastmodified');
+exports.removeOldActivities = functions.database.ref('/{petId}/online').onWrite(event => {
+    const parentRef = event.data.ref.parent;
+    const activitiesRef = parentRef.parent.child('activities');
 
-    return lastmodifiedRef.once('value').then(snapshot => {
-        if (Date.now() - snapshot.val() < 1 * 60 * 60 * 1000) return null;
-        return activitiesRef.once('value').then(snapshot => {
-            const toBeRemoved = {};
-            snapshot.forEach(function(child) {
-                var now = Date.now();
-                var date = Date.parse(child.val().date);
-                if (now - date > 24 * 60 * 60 * 1000) {
-                    toBeRemoved[child.key] = null;
-                }
-            });
-            admin.database().ref(`/${event.params.petId}/lastmodified`).set(Date.now());
-            return activitiesRef.update(toBeRemoved);
+    return activitiesRef.once('value').then(snapshot => {
+        const toBeRemoved = {};
+        snapshot.forEach(function(child) {
+            var now = Date.now();
+            var date = Date.parse(child.val().date);
+            if (now - date > 24 * 60 * 60 * 1000) {
+                toBeRemoved[child.key] = null;
+            }
         });
+        return activitiesRef.update(toBeRemoved);
     });
 });
