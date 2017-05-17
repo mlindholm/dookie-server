2013b36287ddcc305000f81798d7687d56dc272e
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-05-17 16:45:30 +0300

    Add remove old activities function

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,27 @@
-var functions = require('firebase-functions');
+'use strict';
+
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.activityAdded = functions.database.ref('/{pet}/activities/{pushId}')
-    .onWrite(event => {
-        const object = event.data;
-        console.log('Object', object.val())
-        // console.log('Parent', object.ref.parent)
-        // console.log('Data', event.params.pushId, object.val());
+exports.removeOldActivities = functions.database.ref('/{petId}/activities/{pushId}').onWrite(event => {
+    const moment = require('moment');
+    const parentRef = event.data.ref.parent;
+    return parentRef.once('value').then(snapshot => {
+        const updates = {};
+        snapshot.forEach(function(child) {
+            var now = moment();
+            var date = child.val().date;
+            if (now.diff(date, 'days') > 0) {
+                updates[child.key] = null;
+            }
+        });
+        return parentRef.update(updates);
     });
+});
+
+// return admin.database().ref(`/${event.params.petId}/pet/merge`).once('value').then(snapshot => {
+//     var allowedToMerge = snapshot.val();
+//     console.log(event.params.pushId, allowedToMerge);
+//     return allowedToMerge;
+// });
