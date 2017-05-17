f19985c3ca52e31e5b99d6604b13ebd07403cc88
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-05-17 23:23:30 +0300

    Set lastmodified time

--- functions/index.js
+++ functions/index.js
@@ -5,23 +5,22 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.removeOldActivities = functions.database.ref('/{petId}/activities/{pushId}').onWrite(event => {
-    const moment = require('moment');
-    const parentRef = event.data.ref.parent;
-    return parentRef.once('value').then(snapshot => {
-        const updates = {};
-        snapshot.forEach(function(child) {
-            var now = moment();
-            var date = child.val().date;
-            if (now.diff(date, 'days') > 0) {
-                updates[child.key] = null;
-            }
+    const activitiesRef = event.data.ref.parent;
+    const lastmodifiedRef = activitiesRef.parent.child('lastmodified');
+
+    return lastmodifiedRef.once('value').then(snapshot => {
+        if (Date.now() - snapshot.val() < 1 * 60 * 60 * 1000) return null;
+        return activitiesRef.once('value').then(snapshot => {
+            const toBeRemoved = {};
+            snapshot.forEach(function(child) {
+                var now = Date.now();
+                var date = Date.parse(child.val().date);
+                if (now - date > 24 * 60 * 60 * 1000) {
+                    toBeRemoved[child.key] = null;
+                }
+            });
+            admin.database().ref(`/${event.params.petId}/lastmodified`).set(Date.now());
+            return activitiesRef.update(toBeRemoved);
         });
-        return parentRef.update(updates);
     });
 });
-
-// return admin.database().ref(`/${event.params.petId}/pet/merge`).once('value').then(snapshot => {
-//     var allowedToMerge = snapshot.val();
-//     console.log(event.params.pushId, allowedToMerge);
-//     return allowedToMerge;
-// });
